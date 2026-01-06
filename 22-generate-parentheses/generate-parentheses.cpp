572c4eff39c334ee0326063adab1a2c82b8c90b8
Saurav Ratan <ratansaurav31@gmail.com>
2026-01-06 19:09:05 +0530

    Time: 3 ms (71.33%) | Memory: 16.4 MB (16.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    bool isValid(string s){
-        int cnt=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                cnt++;
-            }else{
-                cnt--;
-            }
-            if(cnt<0){
-                return false;
-            }
-        }
-        if(cnt==0){
-            return true;
-        }
-        return false;
-    }
-    void solve(string curr,vector<string>&ans,int n){
+    void solve(string curr,vector<string>&ans,int n,int open,int close){
         //base case
         if(curr.size()==2*n){
-            if(isValid(curr)){
-                ans.push_back(curr);
-            }
+            ans.push_back(curr);
             return;
         }
         //take open
-        curr.push_back('(');
-        solve(curr,ans,n);
-        curr.pop_back();
+        if(open<n){
+             curr.push_back('(');
+            solve(curr,ans,n,open+1,close);
+             curr.pop_back();
+        }
+       
         //take closed
-        curr.push_back(')');
-        solve(curr,ans,n);
-        curr.pop_back();
+        if(close<open){
+             curr.push_back(')');
+            solve(curr,ans,n,open,close+1);
+             curr.pop_back();
+        } 
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
         string s="";
-        solve(s,ans,n);
+        int open=0,close=0;
+        solve(s,ans,n,0,0);
         return ans;
     }
 };
