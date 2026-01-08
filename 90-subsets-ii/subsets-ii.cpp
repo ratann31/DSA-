2ff1757bea90f729660b3649f67349901690dcd4
Saurav Ratan <ratansaurav31@gmail.com>
2026-01-08 17:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (39.35%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    void f(int idx,set<vector<int>>&ans,vector<int>temp,vector<int>& nums){
-        //base case
+    void f(int idx,vector<int>&temp,set<vector<int>>&st,vector<int>&nums){
         if(idx>=nums.size()){
-            //check before pushing into the set
-            if(ans.find(temp)==ans.end()){//not present into the set
-                 ans.insert(temp);
+            if(st.find(temp)==st.end()){
+                st.insert(temp);
             }
             return;
         }
-        //take
         temp.push_back(nums[idx]);
-        f(idx+1,ans,temp,nums);
+        f(idx+1,temp,st,nums);
         temp.pop_back();
-        //not take
-        f(idx+1,ans,temp,nums);
+        f(idx+1,temp,st,nums);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
         sort(nums.begin(),nums.end());
-        set<vector<int>>ans;
+        set<vector<int>>st;
         vector<int>temp;
-        f(0,ans,temp,nums);
-        vector<vector<int>>res;
-        for(auto &it:ans){
-            res.push_back(it);
+        f(0,temp,st,nums);
+        vector<vector<int>>ans;
+        for(auto &it:st){
+            ans.push_back(it);
         }
-        return res;
+        return ans;
+
     }
 };
