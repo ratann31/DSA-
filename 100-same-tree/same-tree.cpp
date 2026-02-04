77ef7d70781a65f2b12a80e48d8aa80c2aa74b0a
Saurav Ratan <ratansaurav31@gmail.com>
2026-02-04 18:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (13.04%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,14 +11,44 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* root1,TreeNode* root2){
-    
-        if(root1==NULL && root2==NULL)return true;//same hai
-        if((root1==NULL && root2!=NULL)||(root1!=NULL && root2==NULL))return false;
-        if(root1->val!=root2->val)return false;
-        return check(root1->left,root2->left) && check(root1->right,root2->right);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-       return check(p,q);
+        //edge cases before starting
+        if((p==NULL && q!=NULL)||(p!=NULL && q==NULL)){
+            return false;
+        }else if(p==NULL && q==NULL){
+            return true;
+        }
+        queue<TreeNode*>q1,q2;
+        q1.push(p),q2.push(q);
+        while(!q1.empty() && !q2.empty()){
+            TreeNode* node1=q1.front();
+            TreeNode* node2=q2.front();
+
+            q1.pop();
+            q2.pop();
+
+            if(node1->val!=node2->val)return false;
+            //left condition
+            if(node1->left!=NULL && node2->left!=NULL){
+                q1.push(node1->left);
+                q2.push(node2->left);
+            }else if((node1->left!=NULL && node2->left==NULL)||
+            (node1->left==NULL && node2->left!=NULL)){
+                return false;
+            }
+            //right condition
+            if(node1->right!=NULL && node2->right!=NULL){
+                q1.push(node1->right);
+                q2.push(node2->right);
+            }else if((node1->right!=NULL && node2->right==NULL)||
+            (node1->right==NULL && node2->right!=NULL)){
+                return false;
+            }
+            
+        }
+        if((!q1.empty()&& q2.empty())||(!q2.empty() && q1.empty())){
+            return false;
+        }
+        return true;
     }
 };
