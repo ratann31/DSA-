95aeabcfb2654172bb1aa1b05b41d06d27f72964
Saurav Ratan <ratansaurav31@gmail.com>
2026-03-09 21:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.94%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -8,14 +8,30 @@ public:
         }
         return res;
     }
+    vector<int>ithRow(int rowNo){
+        long long ans=1;
+        vector<int>res;
+        res.push_back(1);
+        for(int i=1;i<=rowNo;i++){
+            ans=ans*(rowNo-i+1);
+            ans=ans/i;
+            res.push_back(ans);
+        }
+        return res;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
-        for(int row=0;row<numRows;row++){
-            vector<int>temp;
-            for(int col=0;col<=row;col++){
-                temp.push_back(ncr(row,col));
-            }
-            ans.push_back(temp);
+        // for(int row=0;row<numRows;row++){
+        //     vector<int>temp;
+        //     for(int col=0;col<=row;col++){
+        //         temp.push_back(ncr(row,col));
+        //     }
+        //     ans.push_back(temp);
+        // }
+        // return ans;
+        ans.push_back({1});
+        for(int i=1;i<numRows;i++){
+            ans.push_back(ithRow(i));
         }
         return ans;
     }
