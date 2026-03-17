852b077b12c1f31387d4ba3c32ba49b4772b1c0e
Saurav Ratan <ratansaurav31@gmail.com>
2026-03-18 00:03:03 +0530

    Time: 3 ms (29.27%) | Memory: 16.4 MB (15.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -16,7 +16,25 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();//rows
         int n=grid[0].size();//columns
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(0,0,grid,m,n,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return f(0,0,grid,m,n,dp);
+        //tabulation
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        //fill first row and first col of dp
+        dp[0][0]=grid[0][0];
+        for(int j=1;j<n;j++){
+            dp[0][j]=dp[0][j-1]+grid[0][j];
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+        }
+        //fill remaining rows and columns
+        //dp[i][j]=min sum to reach till dp[i][j] fron dp[0][0]
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
