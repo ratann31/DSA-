2496f0e403bdc52358ab098faeaf00fb959a74d9
Saurav Ratan <ratansaurav31@gmail.com>
2026-03-30 17:49:21 +0530

    Time: 3 ms (23.63%) | Memory: 14.5 MB (58.19%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -3,53 +3,49 @@ public:
     int dr[4]={-1,1,0,0};
     int dc[4]={0,0,-1,1};
 
-    bool isEdge(int i,int j,vector<vector<char>>& board){
-        int m=board.size();
-        int n=board[0].size();
+    bool inside(int i,int j,vector<vector<char>>&board){
+        int row=board.size();
+        int col=board[0].size();
 
-        if(i==0 || i==m-1 || j==0 || j==n-1){
-            return true;
+        if(i==0 || i==row-1 || j==0 || j==col-1){
+            return false;//edges  pe hai
         }
-        return false;
+        return true;//andar hai
     }
-
-    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& board){
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&board){
         int m=board.size();
         int n=board[0].size();
+
         vis[row][col]=1;
-        
+
         for(int i=0;i<4;i++){
             int nr=row+dr[i];
             int nc=col+dc[i];
 
-            if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc]=='O' && !vis[nr][nc]){
-                dfs(nr,nc,vis,board);
-            }
+           if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && board[nr][nc]=='O'){
+            
+            dfs(nr,nc,vis,board);
+           }
         }
     }
-
     void solve(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
+        int m=board.size();//no of rows
+        int n=board[0].size();//no of col
+
         vector<vector<int>>vis(m,vector<int>(n,0));
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(isEdge(i,j,board) && board[i][j]=='O'){
+                if(!inside(i,j,board) && !vis[i][j] && board[i][j]=='O'){
                     dfs(i,j,vis,board);
                 }
             }
         }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<vis[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
+        //now all those cells which are not visited should be "X"
 
-        //for every unvisited cells make it 'X'
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j]){
+                if(!vis[i][j] && board[i][j]=='O'){
                     board[i][j]='X';
                 }
             }
