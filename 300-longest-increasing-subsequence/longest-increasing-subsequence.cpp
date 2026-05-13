714578c2606de3c885d1f17c4952bb12ff59baa9
Saurav Ratan <ratansaurav31@gmail.com>
2026-05-14 01:57:57 +0530

    Time: 7 ms (71.61%) | Memory: 16.5 MB (31.06%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        set<int>st;
+        for(int i=0;i<n;i++){
+            if(st.empty() || nums[i]>*st.rbegin()){
+                st.insert(nums[i]);
+            }else{
+                auto it=st.lower_bound(nums[i]);
+                st.erase(it);
+                st.insert(nums[i]);
+            }
+        }
+        return st.size();
+    }
+};
