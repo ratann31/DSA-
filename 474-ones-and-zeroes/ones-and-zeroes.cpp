9e562720843b8a59dd5e51ea6ab6fafca53deafd
Saurav Ratan <ratansaurav31@gmail.com>
2026-05-28 11:59:37 +0530

    Time: 135 ms (46.25%) | Memory: 106.2 MB (21.79%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,74 +1,37 @@
 class Solution {
 public:
-
-    int solve(int idx,
-              int m,
-              int n,
-              vector<pair<int,int>>& cnt,
-              vector<vector<vector<int>>>& dp){
-
-        // base case
-        if(idx < 0){
+    int solve(int idx,vector<pair<int,int>>&cnt,int m,int n,vector<string>&strs,vector<vector<vector<int>>>&dp){
+        if(idx<0){
             return 0;
         }
-
-        // memoization
-        if(dp[idx][m][n] != -1){
+        int zeros=cnt[idx].first;
+        int ones=cnt[idx].second;
+        if(dp[idx][m][n]!=-1){
             return dp[idx][m][n];
         }
-
-        int zero = cnt[idx].first;
-        int one  = cnt[idx].second;
-
-        // skip current string
-        int skip = solve(idx - 1, m, n, cnt, dp);
-
-        // pick current string
-        int pick = 0;
-
-        if(zero <= m && one <= n){
-            pick = 1 + solve(idx - 1,
-                             m - zero,
-                             n - one,
-                             cnt,
-                             dp);
+        int skip=solve(idx-1,cnt,m,n,strs,dp);
+        int pick=0;
+        if(zeros<=m && ones<=n){
+            pick=1+solve(idx-1,cnt,m-zeros,n-ones,strs,dp);
         }
-
-        return dp[idx][m][n] = max(pick, skip);
-    }
-
+        return dp[idx][m][n]=max(pick,skip);
+    }   
     int findMaxForm(vector<string>& strs, int m, int n) {
-
-        int sz = strs.size();
-
-        // precompute zeros and ones
-        vector<pair<int,int>> cnt;
-
-        for(string &s : strs){
-
-            int zero = 0;
-            int one = 0;
-
-            for(char ch : s){
-
-                if(ch == '0'){
-                    zero++;
-                }
-                else{
-                    one++;
+        int sz=strs.size();
+        vector<vector<vector<int>>>dp(sz,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        vector<pair<int,int>>cnt;
+        for(int i=0;i<sz;i++){
+            string temp=strs[i];
+            int z=0,o=0;
+            for(int j=0;j<temp.size();j++){
+                if(strs[i][j]=='0'){
+                    z++;
+                }else{
+                    o++;
                 }
             }
-
-            cnt.push_back({zero, one});
+            cnt.push_back({z,o});
         }
-
-        // 3D DP
-        vector<vector<vector<int>>> dp(
-            sz,
-            vector<vector<int>>(m + 1,
-            vector<int>(n + 1, -1))
-        );
-
-        return solve(sz - 1, m, n, cnt, dp);
+        return solve(sz-1,cnt,m,n,strs,dp);
     }
 };
