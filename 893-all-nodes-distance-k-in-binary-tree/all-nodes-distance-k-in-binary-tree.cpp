9ec79340af9ca3f95fd8706dbf5d98abf395fef7
Saurav Ratan <ratansaurav31@gmail.com>
2026-06-02 12:55:11 +0530

    Time: 12 ms (8.26%) | Memory: 15.7 MB (22.40%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-    void inorder(TreeNode* root,map<TreeNode*,TreeNode*>&parent){
-        if(root==NULL)return;
-        if(root->left!=NULL){
-            parent[root->left]=root;
-        }
-        inorder(root->left,parent);
-        if(root->right!=NULL){
-            parent[root->right]=root;
-        }
-        inorder(root->right,parent);
-    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         vector<int>ans;
-        map<TreeNode*,TreeNode*>parent;
-        map<TreeNode*,bool>visited;
-        inorder(root,parent);
-        queue<TreeNode*>q;
-        q.push(target);
-        visited[target]=true;
+        //making graph
+        unordered_map<int,vector<int>>adj;
+        queue<TreeNode*>pq;
+        pq.push(root);
+        while(!pq.empty()){
+            TreeNode* currNode=pq.front();
+            pq.pop();
+            if(currNode->left){
+                adj[currNode->val].push_back(currNode->left->val);
+                adj[currNode->left->val].push_back(currNode->val);
+                pq.push(currNode->left);
+            }
+            if(currNode->right){
+                adj[currNode->val].push_back(currNode->right->val);
+                adj[currNode->right->val].push_back(currNode->val);
+                pq.push(currNode->right);
+            }
+        }
+        //implementing bfs logic
+        unordered_map<int,bool>vis;
+        queue<int>q;
+        q.push(target->val);
+        vis[target->val]=true;
         int level=0;
         while(!q.empty()){
-            int sz=q.size();
-            vector<int>temp;
-            while(sz--){
-                TreeNode* currNode=q.front();
-                visited[currNode]=true;
-                q.pop();
-                if(currNode->left && !visited[currNode->left]){
-                    visited[currNode->left]=true;
-                    q.push(currNode->left);
-                }
-                if(currNode->right && !visited[currNode->right]){
-                    visited[currNode->right]=true;
-                    q.push(currNode->right);
-                }
-                if(parent.find(currNode)!=parent.end() && !visited[parent[currNode]]){
-                    visited[parent[currNode]]=true;
-                    q.push(parent[currNode]);
-                }
-                temp.push_back(currNode->val);
-            }
-            if(level==k)return temp;
-            level++;
+            int n=q.size();
+             if(level == k){ // collect all nodes at this level
+        while(n--){
+            ans.push_back(q.front());
+            q.pop();
         }
-        return {};
+        break; // stop BFS after distance k
+    }
+            while(n--){
+                int currNode=q.front();
+                q.pop();
+                for(auto &it:adj[currNode]){
+                    if(!vis[it]){
+                        q.push(it);
+                        vis[it]=true;
+                    }
+                    
+                }
+            }
+            level++;
 
+        }
+        return ans;
     }
 };
