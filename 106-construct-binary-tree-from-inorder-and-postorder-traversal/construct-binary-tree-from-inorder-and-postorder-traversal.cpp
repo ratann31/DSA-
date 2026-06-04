386cc806af279215bd2cc731922b62eea566c1cc
Saurav Ratan <ratansaurav31@gmail.com>
2026-06-04 10:40:45 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (56.06%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,40 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* f(vector<int>& inorder, vector<int>& postorder,int start,int end,int &idx){
-        if(start>end){
+    int postIndex;
+    unordered_map<int,int> pos;
+
+    TreeNode* build(vector<int>& postorder,
+                    int inStart,
+                    int inEnd) {
+
+        if(inStart > inEnd)
             return NULL;
-        }
-        int rootVal=postorder[idx];
-        int ix;
-        for(int i=start;i<=end;i++){
-            if(inorder[i]==rootVal){
-                ix=i;
-                break;
-            }
-        }
-        idx++;
-        TreeNode* root=new TreeNode(rootVal);
-       
-        root->right=f(inorder,postorder,ix+1,end,idx);
-        root->left=f(inorder,postorder,start,ix-1,idx);
+
+        int rootVal = postorder[postIndex--];
+
+        TreeNode* root =
+            new TreeNode(rootVal);
+
+        int idx = pos[rootVal];
+
+        root->right =
+            build(postorder,
+                  idx + 1,
+                  inEnd);
+
+        root->left =
+            build(postorder,
+                  inStart,
+                  idx - 1);
 
         return root;
     }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n=postorder.size();
-        reverse(postorder.begin(),postorder.end());
-        int idx=0;
-        return f(inorder,postorder,0,n-1,idx);
+
+    TreeNode* buildTree(vector<int>& inorder,
+                        vector<int>& postorder) {
+
+        int n = inorder.size();
+
+        postIndex = n - 1;
+
+        for(int i = 0; i < n; i++)
+            pos[inorder[i]] = i;
+
+        return build(postorder, 0, n - 1);
     }
 };
