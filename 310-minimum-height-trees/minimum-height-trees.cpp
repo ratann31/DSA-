b4c9ae4825b35ab7dab0b65114ded6902e3cf369
Saurav Ratan <ratansaurav31@gmail.com>
2026-06-08 20:59:10 +0530

    Time: 36 ms (71.32%) | Memory: 56.2 MB (96.94%) - LeetSync

---
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1)
+            return {0};
+
+        vector<vector<int>> adj(n);
+        vector<int> degree(n, 0);
+
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1)
+                q.push(i);
+        }
+
+        int remainingNodes = n;
+
+        while (remainingNodes > 2) {
+
+            int leaves = q.size();
+
+            remainingNodes -= leaves;
+
+            while (leaves--) {
+
+                int node = q.front();
+                q.pop();
+
+                for (int nei : adj[node]) {
+
+                    degree[nei]--;
+
+                    if (degree[nei] == 1)
+                        q.push(nei);
+                }
+            }
+        }
+
+        vector<int> ans;
+
+        while (!q.empty()) {
+            ans.push_back(q.front());
+            q.pop();
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
