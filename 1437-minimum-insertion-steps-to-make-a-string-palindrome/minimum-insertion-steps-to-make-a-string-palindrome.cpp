21559aa7a0d315a44b0e568624f55ce644e38c1d
Saurav Ratan <ratansaurav31@gmail.com>
2026-06-17 15:33:02 +0530

    Time: 36 ms (32.90%) | Memory: 31.4 MB (58.87%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int i,int j,string &s,vector<vector<int>>&dp){
+        if(i>=j)return 0;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==s[j]){
+           return dp[i][j]=solve(i+1,j-1,s,dp);   
+        }
+        return dp[i][j]=min(1+solve(i+1,j,s,dp),1+solve(i,j-1,s,dp));
+        
+    }
+    int minInsertions(string s) {
+        int n=s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(0,n-1,s,dp);
+    }
+};
