d55d520b9a2ab4c150f4e8b8f605acf572bc25bc
Saurav Ratan <ratansaurav31@gmail.com>
2026-06-20 16:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.04%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int l = 0;
+
+        for(int r = 0; r < n; r++) {
+            if(nums[r] != 0) {
+                swap(nums[l], nums[r]);
+                l++;
+            }
+        }
+    }
+};
