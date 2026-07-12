02079df642b05f829548233963f4e98d17328efa
Saurav Ratan <ratansaurav31@gmail.com>
2026-07-12 14:49:20 +0530

    Time: 18 ms (83.99%) | Memory: 17.5 MB (55.46%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n = nums.size();
+        vector<vector<int>> ans;
 
-        sort(nums.begin(),nums.end());
-        set<vector<int>>st;
-        vector<vector<int>>ans;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int k=j+1;
-                int l=n-1;
+        sort(nums.begin(), nums.end());
 
-                while(k<l){
-                    long long  currSum=1LL*nums[i]+nums[j]+nums[k]+nums[l];
+        for(int i = 0; i < n; i++) {
 
-                    if(currSum == target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
 
-                        st.insert(temp);
+            for(int j = i + 1; j < n; j++) {
+
+                if(j > i + 1 && nums[j] == nums[j-1])
+                    continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while(k < l) {
+
+                    long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
                         k++;
                         l--;
-                    }else if(currSum < target){
+
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum < target) {
                         k++;
-                    }else{
+                    }
+                    else {
                         l--;
                     }
                 }
             }
         }
-        for(auto &it:st){
-            ans.push_back(it);
-        }
+
         return ans;
     }
 };
