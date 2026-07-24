bf9f161e86500f9325f89c0328a27a82b5fa575c
Saurav Ratan <ratansaurav31@gmail.com>
2026-07-24 21:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (90.04%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    long long ncr(int row,int col){
-        long long res=1;
-        for(int i=0;i<col;i++){
-            res=res*(row-i);
-            res=res/(i+1);
-        }
-        return res;
-    }
     vector<int>ithRow(int rowNo){
         long long ans=1;
         vector<int>res;
@@ -21,14 +13,6 @@ public:
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
-        // for(int row=0;row<numRows;row++){
-        //     vector<int>temp;
-        //     for(int col=0;col<=row;col++){
-        //         temp.push_back(ncr(row,col));
-        //     }
-        //     ans.push_back(temp);
-        // }
-        // return ans;
         ans.push_back({1});
         for(int i=1;i<numRows;i++){
             ans.push_back(ithRow(i));
