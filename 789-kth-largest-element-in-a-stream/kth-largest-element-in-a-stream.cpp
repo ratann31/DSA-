b63c469e9ae30b723d5822373e4cb9a84a4adba0
Saurav Ratan <ratansaurav31@gmail.com>
2026-07-28 16:27:37 +0530

    Time: 11 ms (28.34%) | Memory: 33.2 MB (26.87%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,25 +1,24 @@
 class KthLargest {
 public:
-    //min-Heap for streams
-    int k;
+    int K;
     priority_queue<int,vector<int>,greater<int>>pq;
-
     KthLargest(int k, vector<int>& nums) {
-        this->k=k;
-        for(int i=0;i<nums.size();i++){
+        K=k;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
             pq.push(nums[i]);
-            if(pq.size()>k){
+            if(pq.size()>K){
                 pq.pop();
             }
         }
     }
     
     int add(int val) {
-       pq.push(val);
-       if(pq.size()>k){
-        pq.pop();
-       }
-       return pq.top();
+        pq.push(val);
+        while(!pq.empty() && pq.size()>K){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
 
