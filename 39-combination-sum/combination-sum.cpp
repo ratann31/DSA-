1afc8fcc2561b133e004001595236dc8224d9be5
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-04 09:37:17 +0530

    Time: 2 ms (58.64%) | Memory: 14.2 MB (59.80%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    void solve(int idx,int t,vector<int>&ds,vector<vector<int>>&ans,vector<int>& candidates){
-        if(idx>=candidates.size() || t<0){
+    void f(int idx, vector<vector<int>>& ans, vector<int>& temp,
+           vector<int>& candidates, int target) {
+
+        if (target == 0) {
+            ans.push_back(temp);
             return;
         }
-        if(t==0){
-           ans.push_back(ds);
-           return;
+
+        if (idx == candidates.size())
+            return;
+
+        // Take current element
+        if (candidates[idx] <= target) {
+            temp.push_back(candidates[idx]);
+            f(idx, ans, temp, candidates, target - candidates[idx]);
+            temp.pop_back();
         }
-        //pick
-        ds.push_back(candidates[idx]);
-        solve(idx,t-candidates[idx],ds,ans,candidates);
-        ds.pop_back();
-        //not pick
-        solve(idx+1,t,ds,ans,candidates);
+
+        // Skip current element
+        f(idx + 1, ans, temp, candidates, target);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n=candidates.size();
-        vector<vector<int>>ans;
-        vector<int>ds;
-        solve(0,target,ds,ans,candidates);
+        vector<vector<int>> ans;
+        vector<int> temp;
+        f(0, ans, temp, candidates, target);
         return ans;
     }
 };
