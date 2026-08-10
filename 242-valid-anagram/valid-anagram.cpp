8239a66051f0ec853847793650e2e610cbeb8647
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-10 15:44:42 +0530

    Time: 2 ms (50.13%) | Memory: 10 MB (11.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            char ch=t[i];
+            if(mpp.find(ch)==mpp.end()){
+                return false;
+            }
+            else{
+                if(mpp[ch]==1){
+                    mpp.erase(ch);
+                }else{
+                    mpp[ch]-=1;
+                }
+            }
+        }
 
-        return s==t;
+        return true;
     }
 };
