6e5338d8e98f9e626c2d0dffd70666c72470b80e
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-13 13:46:11 +0530

    Time: 7 ms (4.79%) | Memory: 27.2 MB (26.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        /*
-        1.find left max and right max for each arr[i]
-        2.for each arr[i]....water trapped will be : min(leftmax[i],rightmax[i])-arr[i]
-        3.ans=sum of all water trapped
-        */
         int n=height.size();
+
+        //step-1:LeftMax
         int l=INT_MIN;
-        vector<int>lmax(n,0);
+        vector<int>leftMax(n,0);
         for(int i=0;i<n;i++){
-            lmax[i]=max(l,height[i]);
+            leftMax[i]=max(l,height[i]);
             l=max(l,height[i]);
         }
+        //step-2:RightMax
         int r=INT_MIN;
-        vector<int>rmax(n,0);
+        vector<int>rightMax(n,0);
         for(int i=n-1;i>=0;i--){
-            rmax[i]=max(r,height[i]);
+            rightMax[i]=max(r,height[i]);
             r=max(r,height[i]);
         }
-        int ans=0;
+
+        int totalTrappedWater=0;
         for(int i=0;i<n;i++){
-           ans+=max(0,min(lmax[i],rmax[i])-height[i]);
+            int minHeight = min(leftMax[i],rightMax[i]);
+            totalTrappedWater += max(0,minHeight-height[i]);
         }
-        return ans;
+
+        return totalTrappedWater;
     }
 };
