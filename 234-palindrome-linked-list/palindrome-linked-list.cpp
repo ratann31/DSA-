8a5b6ff194c987405dd89b22a45cede5e3a71b3c
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-16 01:17:18 +0530

    Time: 3 ms (58.47%) | Memory: 118.2 MB (51.73%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,26 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
-       string s="";
-       ListNode* curr=head;
-       while(curr!=NULL){
-        int currNode=curr->val;
-        char ch=currNode+'0';
-        s+=ch;
-        curr=curr->next;
-       }
-       string temp=s;
-       reverse(s.begin(),s.end());
-       return temp==s;
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* secondHalf = reverseLL(slow);
+
+        // Compare first half and reversed second half
+        ListNode* firstHalf = head;
+
+        while (secondHalf != NULL) {
+            if (firstHalf->val != secondHalf->val)
+                return false;
+
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
     }
 };
