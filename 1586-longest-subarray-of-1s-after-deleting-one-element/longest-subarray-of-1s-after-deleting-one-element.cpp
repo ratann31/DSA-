7568891d598f9e75f384a1e581e960005ce02484
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-16 21:42:03 +0530

    Time: 18 ms (6.33%) | Memory: 60.3 MB (13.24%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        //longest subarray containing at max one '0'
         int n=nums.size();
-        int ans=0;
+        unordered_map<int,int>mpp;
         int l=0,r=0;
-        int z=0;
+        int maxLen=0;
+
         while(r<n){
-            if(nums[r]==0){
-                z++;
+            mpp[nums[r]]++;
+
+            while(mpp[0]>1){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0){
+                    mpp.erase(nums[l]);
+                }
+                l++;
             }
-            while(z>1){
-                if(nums[l]==0){
-                    z--;
-                }l++;
-            }
-            ans=max(ans,r-l+1);
+
+            maxLen=max(maxLen,r-l+1);
             r++;
         }
-        return ans-1;
+
+        return maxLen-1;
+
     }
 };
