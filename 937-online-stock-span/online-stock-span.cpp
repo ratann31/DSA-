12f1176ccc8d6c61af6c6465e8cebec67e5edeef
Saurav Ratan <ratansaurav31@gmail.com>
2026-08-26 00:58:28 +0530

    Time: 16 ms (97.33%) | Memory: 97 MB (8.53%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,16 +1,19 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>>st;//{price,span} 
+    stack<pair<int,int>>st; //{current price,span of current Price}
     StockSpanner() {
         
     }
+    
     int next(int price) {
         int span=1;
-        while(!st.empty() && st.top().first <= price){
-            span=span+st.top().second;
+
+        while(!st.empty() && st.top().first<=price){
+            span+=st.top().second;
             st.pop();
         }
         st.push({price,span});
+
         return span;
     }
 };
