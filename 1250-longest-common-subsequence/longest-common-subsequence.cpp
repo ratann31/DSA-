6c6099da5e2f0b93e1b4e5b5acb41d01ae6bcda2
Saurav Ratan <ratansaurav31@gmail.com>
2026-09-15 11:19:13 +0530

    Time: 41 ms (30.50%) | Memory: 27.8 MB (7.32%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
+    int f(int idx1,int idx2,string &s1,string &s2,vector<vector<int>>&dp){
+        if(idx1<0 || idx2<0)return 0;
+        if(dp[idx1][idx2]!=-1){
+            return dp[idx1][idx2];
+        }
+        int len=0;
+
+        if(s1[idx1]==s2[idx2]){
+            len=1+f(idx1-1,idx2-1,s1,s2,dp);
+        }else{
+            int len1=f(idx1-1,idx2,s1,s2,dp);
+            int len2=f(idx1,idx2-1,s1,s2,dp);
+
+            len=max(len1,len2);
+        }
+
+        return dp[idx1][idx2] = len;
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size();
-        int n=text2.size();
-
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-
-        dp[0][0]=0;
-        for(int i=0;i<m;i++){
-            dp[i][0]=0;
-        }
-        for(int j=0;j<n;j++){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        
-
-        int i=m,j=n;
-        string lcs="";
-        while(i>0 && j>0){
-            if(text1[i-1]==text2[j-1]){
-                lcs.push_back(text1[i-1]);
-                i--;
-                j--;
-            }else{
-                //pata kro konsa bada hai upper wala ya left wala
-                if(dp[i-1][j]>dp[i][j-1]){
-                    i--;
-                }else{
-                    j--;
-                }
-            }
-        }
-        reverse(lcs.begin(),lcs.end());
-        cout<<lcs<<endl;
-
-        return dp[m][n];
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return f(n1-1,n2-1,text1,text2,dp);
     }
 };
