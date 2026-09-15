252570576a4b31a4add8771d999b2ebba49601a6
Saurav Ratan <ratansaurav31@gmail.com>
2026-09-15 10:59:05 +0530

    Time: 6 ms (9.66%) | Memory: 16.4 MB (12.44%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int f(int idx,int buyOrSell,vector<int>&prices,vector<vector<int>>&dp){
-        //base case
-        if(idx>=prices.size()){
-            return 0;
-        }
-        if(dp[idx][buyOrSell]!=-1){
-            return dp[idx][buyOrSell];
+    int f(int idx,int canBuy,vector<int>&prices,vector<vector<int>>&dp){
+        if(idx>=prices.size())return 0;
+
+        if(dp[idx][canBuy]!=-1){
+            return dp[idx][canBuy];
         }
         int profit=0;
-        if(buyOrSell==0){
-            int buy=-prices[idx]+f(idx+1,1,prices,dp);
-            int notBuy=f(idx+1,0,prices,dp);
 
-             profit=max(buy,notBuy);
+        if(canBuy==1){
+            int buy=-prices[idx]+f(idx+1,0,prices,dp);
+            int notBuy=f(idx+1,1,prices,dp);
+
+            profit=max(buy,notBuy);
         }else{
-            int sell = prices[idx] + f(idx+2,0,prices,dp);
-            int notSell = f(idx+1,1,prices,dp);
+            int sell=prices[idx]+f(idx+2,1,prices,dp);
+            int notSell=f(idx+1,0,prices,dp);
 
-             profit=max(sell,notSell);
+            profit=max(sell,notSell);
         }
 
-        return dp[idx][buyOrSell] = profit;
+        return dp[idx][canBuy] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return f(0,0,prices,dp);
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return f(0,1,prices,dp);
     }
 };
